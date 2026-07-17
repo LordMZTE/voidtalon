2eca676de37e004eb41ebd768d17467b372b0273
LordMZTE <lord@mzte.de>
2026-07-17 19:35:17 +0200

    fix: replace tabs with spaces before rendering
    This used to break Brick.

--- src/VoidTalon/Net/Completions.hs
+++ src/VoidTalon/Net/Completions.hs
@@ -150,7 +150,15 @@ instance FromJSON CompletionChoice where
     d <- v .: "delta"
     CompletionChoice
       <$> ( LLMMessage
-              <$> d .:? "reasoning_content" .!= ""
-              <*> d .:? "content" .!= ""
+              <$> (untab <$> d .:? "reasoning_content" .!= "")
+              <*> (untab <$> d .:? "content" .!= "")
           )
+    where
+      -- | Replaces all tabs with four spaces.  We do this because feeding tabs to VTY causes
+      -- breakage, and it's cheapest to do here where all strings are still short.
+      --
+      -- Note that in rare cases, this breaks syntax highlighting with formats that are picky with
+      -- spaces and tabs.  Try to make the AI generate a makefile, and you'll get what I mean.
+      untab :: T.Text -> T.Text
+      untab = T.replace "\t" "    "
   parseJSON _ = mempty
